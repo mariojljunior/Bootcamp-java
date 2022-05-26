8446484fdc39684dc894acfb57cfac417d65ba7a
Mario Junior <65376066+mariojljunior@users.noreply.github.com>
2022-05-25 23:00:19 -0400

    Adcionando os atributos das classes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição da mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
